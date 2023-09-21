f7f168117e0e1be457c5ed021d7cd5cc72ceb879
OleksanderMelnik <alekandrmelnik@gmail.com>
2023-09-21 16:14:14 +0300

    homevork 10

--- src/index.js
+++ src/index.js
@@ -1,6 +1,70 @@
 import { fetchBreeds, fetchCatByBreed } from "./cat-api";
+import './styles.css';
+import { Notify } from 'notiflix/build/notiflix-notify-aio';
+import SlimSelect from 'slim-select'
+import 'slim-select/dist/slimselect.css';
+
+const refs = {
+    selector: document.querySelector('.breed-select'),
+    catInfo: document.querySelector('.cat-info'),
+    loader: document.querySelector('.loader'),
+    error: document.querySelector('.error'),
+};
+
+const { selector, catInfo, loader, error } = refs;
+
+loader.classList.replace('loader', 'is-hidden');
+error.classList.add('is-hidden');
+catInfo.classList.add('is-hidden');
+let arrayId = [];
 
 
+fetchBreeds().then(data => {
+    data.forEach(e => {
+        arrayId.push({
+            text: e.name, 
+            value: e.id});
+        // console.log(e);
+        // console.log(e.name);
+        // console.log(e.id);
+    });  
+    
+    new SlimSelect({
+        select: selector,
+        data: arrayId
+    });
+    // console.log(selector);
+    })
+.catch(fetchError);
+
+selector.addEventListener('change', selectBreed);
+
+function selectBreed(e) {
+    loader.classList.replace('is-hidden', 'loader');
+    selector.classList.add('is-hidden');
+    catInfo.classList.add('is-hidden');
+    breedId = e.currentTarget.value;
+
+    // console.log(breedId);
+
+    fetchCatByBreed(breedId).then(data => {
+        loader.classList.replace('loader', 'is-hidden');
+        selector.classList.remove('is-hidden');
+        const { url, breeds } = data[0];
+        catInfo.innerHTML = `<div class="box-img"><img src="${url}" alt="${breeds[0].name}" width="400"/></div><div class="box"><h1>${breeds[0].name}</h1><p>${breeds[0].description}</p><p><b>Temperament:</b> ${breeds[0].temperament}</p></div>`
+        catInfo.classList.remove('is-hidden');
+    })
+    .catch(fetchError);
+};
+
+
+
+
+function fetchError() {
+    selector.classList.remove('is-hidden');
+    loader.classList.replace('loader', 'is-hidden');
+    Notify.warning('Oops! Something went wrong! Choose another cat breed!');
+};
 
 
 
