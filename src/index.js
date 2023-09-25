21333bebdc93c82eb952949014d164de00025e43
OleksanderMelnik <alekandrmelnik@gmail.com>
2023-09-25 13:37:02 +0300

    delete background-color

--- src/index.js
+++ src/index.js
@@ -16,7 +16,7 @@ let breedArr = [];
 
 loader.classList.toggle('loader');
 error.classList.add('is-hidden');
-// catInfo.classList.add('is-hidden');
+catInfo.classList.remove('cat-info');
 
 fetchBreeds().then(data => {
     loader.classList.add('loader-text')
@@ -38,13 +38,10 @@ selector.addEventListener('change', selectBreed);
 
 function selectBreed(e) {
     loader.classList.replace('is-hidden', 'loader');
-    // selector.classList.add('is-hidden');
-    catInfo.classList.add('is-hidden');
-
+    catInfo.classList.add('cat-info');
     const breedId = e.currentTarget.value;
     fetchCatByBreed(breedId).then(data => { 
         loader.classList.add('loader', 'is-hidden');
-        // selector.classList.remove('is-hidden');
         const { url, breeds } = data[0];
         catInfo.innerHTML = `<div class="box-img"><img src="${url}" alt="${breeds[0].name}" width="400"/></div><div class="box"><h1>${breeds[0].name}</h1><p>${breeds[0].description}</p><p><b>Temperament:</b> ${breeds[0].temperament}</p></div>`
         catInfo.classList.remove('is-hidden');
@@ -54,8 +51,7 @@ function selectBreed(e) {
 
 
 function fetchError() {
-    // selector.classList.toggle('is-hidden');
-    // loader.classList.toggle('loader', 'is-hidden');
+
     Notify.warning('Oops! Something went wrong! Choose another cat breed!', {
         timeout: 5000,
     });   
